b27137fae9e4bf17d70eec2e6d961e6256c319d4
DESKTOP-VBT38QC\Admin <swatigadhave86@gmail.com>
2024-09-01 15:06:07 +0530

    Added reverse string program

---
+++ src/ReverseString.java
@@ -0,0 +1,29 @@
+
+
+
+// Reversing the string
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String name="swati";
+		
+		int len = name.length();
+		
+		for(int k=len-1;k>=0; k--)
+		{
+			System.out.print(name.charAt(k));
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
